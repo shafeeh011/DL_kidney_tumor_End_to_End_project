8e99ab2bd8b22d54dd604c15cb1366d71e169bb4
shafeeh011 <shafeeh011@.com>
2025-02-11 16:22:49 +0530

    model trained

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.dlproject import logger
 from src.dlproject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.dlproject.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from src.dlproject.pipeline.stage_03_modle_training import ModelTrainingTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -19,6 +20,17 @@ STAGE_NAME = "Prepare base model stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Training stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainingTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
